8a6f544d7e9056235a3be27dccbeec13a742aad1
LucasNahuelGarcia <nahuellucasgarcia@gmail.com>
2023-01-10 20:24:18 -0300

    Moved cubiclewall and trash to office prefab 	Refactor PlayerMovement and CameraMovement

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.AI;
+
+public class CharacterMovement : MonoBehaviour
+{
+    Controls controls;
+    NavMeshAgent navMeshAgent;
+    Animator animator;
+    private void Awake()
+    {
+        controls = new Controls();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+    }
+
+    public void SetDestination(Vector3 destination)
+    {
+        navMeshAgent.SetDestination(destination);
+    }
+
+    private void Update()
+    {
+        animator.SetFloat("Speed", navMeshAgent.velocity.magnitude);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,26 +6,49 @@ using UnityEngine.AI;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Transform pivot;
+    public float sensitivity = 1f;
     Controls controls;
-    NavMeshAgent navMeshAgent;
+    [SerializeField] CharacterMovement Character;
     [SerializeField] Camera mainCamera;
+
     private void Awake()
     {
+        if (Character == null)
+            Debug.LogWarning("No player set");
+        if (pivot == null)
+            Debug.LogWarning("No pivot set for camera movement");
         controls = new Controls();
-        navMeshAgent = GetComponent<NavMeshAgent>();
-        mainCamera = mainCamera ?? Camera.main;
+        mainCamera = this.GetComponent<Camera>();
     }
+
+    private void OnEnable()
+    {
+        controls.Gameplay.Enable();
+        controls.Gameplay.MoveCamera.performed += OnMove;
+        controls.Gameplay.ActionOnPoint.performed += ActionOnPoint;
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        Vector2 value = context.ReadValue<Vector2>();
+        Debug.Log($"Move Input: {value}");
+
+
+        this.transform.RotateAround(pivot.position, Vector3.up, value.x * sensitivity);
+    }
+
 
     private void ActionOnPoint(InputAction.CallbackContext context)
     {
-        Vector2 mousePosition = context.ReadValue<Vector2>();
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
 
         Debug.Log($"Clicked on: {mousePosition}");
         Ray ray = mainCamera.ScreenPointToRay(mousePosition);
 
-        if (Physics.Raycast(ray, out RaycastHit hit, 10f))
+        if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            navMeshAgent.destination = hit.point;
+            Character.SetDestination(hit.point);
         }
     }
 }
